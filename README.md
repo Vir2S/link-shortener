93efa438d611dc5e5fe8796a2c4526db21c53791
Vitaly Sem <virtualik@gmail.com>
2020-07-25 02:30:35 +0300

    description fixed

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # link-shortener
-Python Django Link Shortener
+Python Django Link Shortener REST API
+
+Easy to create short link from any url.
+
+# Endpoints:
+    - url shortener:
+        /api/shortener/(?P<origin_uri>.+)$
+        method POST
+        payload_to_create={'url': url, 'short_url': short_url}
+        response serialized data
+
+    - export urls list to csv-file:
+        /api/export/
+        method GET
+
+    - url hash:
+        /api/(?P<hash>.+)$
+        method GET
+        response hash redirect
+        
+    - urls list:
+        /api/
+        method GET
+        response serialized data
--- linkshortener/linkshortener/urls.py
+++ linkshortener/linkshortener/urls.py
@@ -1,18 +1,3 @@
-"""linkshortener URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, re_path
 from rest_framework.routers import DefaultRouter
@@ -20,13 +5,13 @@ from app.views import UrlListViewSet, UrlShortener, UrlExport, ShortUrlView
 
 
 router = DefaultRouter()
-router.register('list', UrlListViewSet)
+router.register('api', UrlListViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^shortener/(?P<origin_uri>.+)$', UrlShortener.as_view()),
-    path('export/', UrlExport.as_view()),
-    re_path(r'^(?P<hash>.+)$', ShortUrlView.as_view()),
+    re_path(r'^api/shortener/(?P<origin_uri>.+)$', UrlShortener.as_view()),
+    path('api/export/', UrlExport.as_view()),
+    re_path(r'^api/(?P<hash>.+)$', ShortUrlView.as_view()),
 ]
 
 urlpatterns += router.urls
