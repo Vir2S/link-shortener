2e17a6b733c53fb3b83a84fc0cd0763ffc314884
Vitaly Sem <virtualik@gmail.com>
2020-07-24 19:57:32 +0300

    routers added

--- linkshortener/linkshortener/urls.py
+++ linkshortener/linkshortener/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from app.views import UrlListViewSets
+
+
+router = DefaultRouter()
+router.register('', UrlListViewSets)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
