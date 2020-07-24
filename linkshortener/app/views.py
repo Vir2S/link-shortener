24ae4a77b023cb443480ce5b02304d81cd9e5f26
Vitaly Sem <virtualik@gmail.com>
2020-07-24 20:24:59 +0300

    uri fixed

--- linkshortener/app/views.py
+++ linkshortener/app/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, mixins
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Url
 from .serializers import UrlSerializer
 
@@ -6,3 +8,16 @@ from .serializers import UrlSerializer
 class UrlListViewSets(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = UrlSerializer
     queryset = Url.objects.all()
+
+
+class UrlShorter(APIView):
+    def post(self, request, origin_uri):
+        try:
+            url = Url.objects.get(url=origin_uri)
+        except:
+            url = Url(url=origin_uri)
+            url.save()
+
+        short_url = url.short_url
+
+        return Response(short_url)
