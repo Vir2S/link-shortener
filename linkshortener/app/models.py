31c0008e286df07dc5ead59275d2331bbed6e50e
Vitaly Sem <virtualik@gmail.com>
2020-07-24 20:30:04 +0300

    save method fixed

--- linkshortener/app/models.py
+++ linkshortener/app/models.py
@@ -15,16 +15,21 @@ class Url(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
 
     def save_url(self, *args, **kwargs):
+        self.hash_url = self.hash_generator()
+        self.short_url = self.short_url_creator()
         super(Url, self).save(*args, **kwargs)
 
     def hash_generator(self):
         hash = base64.urlsafe_b64decode(uuid.uuid1().bytes)[:8]
         hash_exist = Url.objects.filter(hash_url=hash)
+
         while hash_exist:
             hash = base64.urlsafe_b64decode(uuid.uuid1().bytes)[:8]
             hash_exist = Url.objects.filter(hash_url=hash)
             continue
+
         hash = hash.decode('utf-8')
+        return hash
 
     def short_url_creator(self):
         return HOST_NAME + self.hash_url
